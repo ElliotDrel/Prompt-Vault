f7fe4432cd18e2fb0b2e0f6695e4874f98d119eb
Elliot Drel <156480527+ElliotDrel@users.noreply.github.com>
2026-01-13 22:51:44 -0500

    Disable browser scroll restoration on route change

--- src/components/ScrollToTopOnRoute.tsx
+++ src/components/ScrollToTopOnRoute.tsx
@@ -1,10 +1,23 @@
-import { useEffect } from "react";
+import { useEffect, useLayoutEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 const ScrollToTopOnRoute = () => {
   const location = useLocation();
 
   useEffect(() => {
+    if (!("scrollRestoration" in window.history)) {
+      return;
+    }
+
+    const originalScrollRestoration = window.history.scrollRestoration;
+    window.history.scrollRestoration = "manual";
+
+    return () => {
+      window.history.scrollRestoration = originalScrollRestoration;
+    };
+  }, []);
+
+  useLayoutEffect(() => {
     window.scrollTo({ top: 0, left: 0 });
   }, [location.pathname]);
 
