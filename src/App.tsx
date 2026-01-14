b6306eb2498d51f503afea5a99b2bfef099a06b8
Elliot Drel <156480527+ElliotDrel@users.noreply.github.com>
2026-01-13 22:54:17 -0500

    Use router scroll restoration

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { createBrowserRouter, createRoutesFromElements, Outlet, Route, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, createRoutesFromElements, Outlet, Route, RouterProvider, ScrollRestoration } from "react-router-dom";
 import { AuthProvider } from "@/contexts/AuthContext";
 import { PromptsProvider } from "@/contexts/PromptsContext";
 import { CopyHistoryProvider } from "@/contexts/CopyHistoryContext";
@@ -11,7 +11,6 @@ import { RequireAuth } from "@/components/auth/RequireAuth";
 import { Toaster as HotToaster } from 'react-hot-toast';
 import { Analytics } from "@vercel/analytics/react";
 import { ScrollToTop } from "@/components/ScrollToTop";
-import ScrollToTopOnRoute from "@/components/ScrollToTopOnRoute";
 import Index from "./pages/Index";
 import CopyHistory from "./pages/CopyHistory";
 import Auth from "./pages/Auth";
@@ -46,9 +45,9 @@ const RootLayout = () => (
       }}
     />
     <Outlet />
+    <ScrollRestoration />
     <Analytics />
     <ScrollToTop />
-    <ScrollToTopOnRoute />
   </>
 );
 
